7f34fad1092859c26ba10af16ee90b9907d7e4e2
Wael Farhan <wael34218@gmail.com>
2015-05-09 16:09:23 -0700

    PCA dumb implementation- should work- TODO rewrite

--- common/pca_images.m
+++ common/pca_images.m
@@ -1,8 +1,7 @@
 function [transformed_matrix] = pca_images...
-    (dataset, folder, n_components, force)
+    (cellmatrix, n_components, force)
 % Input
-% dataset = (POFA|NimStim)
-% folder = (original|rescaled|zscored...)
+% cellmatrix = Input cell matrix
 % n_components = degrees of freedom
 % force = (default = false | true) recalculate if necessary
 %
@@ -14,19 +13,14 @@ function [transformed_matrix] = pca_images...
 
     % Set Default Parameters
     if nargin < 2
-        folder = 'zscored';
-    end
-    if nargin < 3
         n_components = 8;
     end
-    if nargin < 4
+    if nargin < 3
         force = false;
     end
 
-    path = strcat('../', dataset, '/', folder, '/');
     target = strcat('../', dataset, '/pca/');
-    filename = strcat(target, folder, '_',...
-        num2str(n_components),'.mat');
+    filename = strcat('pca_',num2str(n_components),'.mat');
     
     if ~exist(target, 'dir')
         mkdir(target);
@@ -35,34 +29,38 @@ function [transformed_matrix] = pca_images...
     if force && exist(filename, 'file')
         delete(filename);
     end
-
+    
+    
     if exist(filename, 'file') == 2
         fprintf('PCA data loaded from file.\n');
         loaded = load(filename, 'transformed_matrix');
         transformed_matrix = loaded.transformed_matrix;
     else
-        fprintf('Calculating PCA.\n');
-        files = dir(path);
-        
+        fprintf('Calculate PCA.\n');
         images = [];
-        vector_length = 0;
-        for file = files'
-            if file.isdir || strcmp(file.name, '.gitignore') ...
-                    || strcmp( file.name, '.DS_Store')
-                continue;
+        [w h] = cellmatrix(1,1,1);
+        vector_length = w * h;
+        for n = 1:size(cellmatrix, 1)
+            for s = 1:size(cellmatrix, 2)
+                image = [];
+                for j = 1:size(cellmatrix, 3)
+                    i = double(reshape(cellmatrix(n, s, j),[1 vector_length]));
+                    image = [image; i];
+                end
+                images = [images; image];
             end
-            
-            image = imread(strcat(path, file.name));
-            if vector_length == 0
-                [w, h] = size(image);
-                vector_length = w * h;
-            end
-            
-            images = [images; double(reshape(image,[1 vector_length]))];
         end
+
         [coeff, score] = pca(images);
-        transformed_matrix = score(:,1:n_components);
-        
+        temp_matrix = score(:,1:n_components);
+
+        transformed_matrix = cell(size(cellmatrix, 1), size(cellmatrix, 2));
+        for n = 1:size(cellmatrix, 1)
+            for s = 1:size(cellmatrix, 2)
+                index = (n-1) * s + s;
+                transformed_matrix(n, s) = temp_matrix(index,:);
+            end
+        end
         save(filename, 'transformed_matrix');
     end
 end
