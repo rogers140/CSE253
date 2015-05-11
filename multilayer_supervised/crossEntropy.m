3da833ca128399f1ab334152c9aae5ca168191fa
rogers140 <rogers140@gmail.com>
2015-05-11 16:08:41 -0700

    add cross_entropy

---
+++ multilayer_supervised/crossEntropy.m
@@ -0,0 +1,11 @@
+function cost = crossEntropy(output, label)
+%output is data_size*class_num matrix before softmax (directly from network)
+%label is data_sizeclass_size matrix for real label
+%it will give output of crossEntropy
+num_classes = size(output,2);
+temp = exp(output);
+denominator = repmat(sum(temp, 2), [1, num_classes]);
+temp = temp ./ denominator; %softmax
+cost_matrix = (-1)*log(temp)*label';
+cost = sum(diag(cost_matrix));%we only needs to sum the diagnal value
+end
